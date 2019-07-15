d8b2d5d2757284acd15a2994eef3a5ddeec38999
Shahbaz Khan <ai.engg.shahbaz@gmail.com>
2019-07-15 19:55:37 +0530

    Add files via upload

--- run.py
+++ run.py
@@ -2,6 +2,9 @@ from wrappers import make_env
 import torch
 import torch.nn as nn
 import numpy as np
+import tensorboardX
+
+writer = tensorboardX.SummaryWriter('./logs/')
 
 
 class PolicyNet(nn.Module):
@@ -9,9 +12,9 @@ class PolicyNet(nn.Module):
     def __init__(self, input_shape, n_actions):
         super(PolicyNet, self).__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(input_shape[0], 1, kernel_size=8, stride=4),
+            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
             nn.ReLU(),
-            nn.Conv2d(1, 64, kernel_size=4, stride=2),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
             nn.ReLU(),
             nn.Conv2d(64, 64, kernel_size=3, stride=1),
             nn.ReLU()
@@ -62,23 +65,31 @@ class Agent:
             trajectories = []
             ITER_REW = 0
             for _ in range(num_traj):
-                trajectory = []
+                rewards = []
+                log_probs = []
                 s = env.reset()
                 done = False
                 while not done:
                     s = torch.FloatTensor([s]).cuda()
-                    u = self.policy_net(s).detach().cpu().numpy()
+                    a = self.policy_net(s)
+                    del s
+                    a2 = a.detach().cpu().numpy()
                     vec = [0, 1, 2, 3, 4, 5]
-                    u = np.random.choice(vec, 1, replace=False, p=u[0])
-                    sp, r, done, _ = env.step(u)
+                    u = np.random.choice(vec, 1, replace=False, p=a2[0])
+                    log_probs.append(a[0][u])
+                    del a
+                    sp, r, done, _ = env.step(u[0])
                     ITER_REW += r
+                    rewards.append(r)
                     # env.render()
-                    trajectory.append({'s': s, 'u': u, 'r': r})
                     s = sp
-                trajectories.append(trajectory)
+                trajectories.append({'log_probs': log_probs, 'rewards': rewards})
             # self.update_baseline(base_epochs, trajectories, gamma)
             self.update_policy(trajectories, gamma)
             print("ITERATION:", iter+1, "AVG REWARD:", ITER_REW/num_traj)
+            writer.add_scalar("avg reward", ITER_REW/num_traj, iter)
+            torch.save(self.policy_net.state_dict(), './policynet')
+            # torch.save(slbase_net.state_dict(), './basenet')
 
     def update_baseline(self, epochs, trajectories, gamma):
         criterion = torch.nn.MSELoss()
@@ -99,35 +110,41 @@ class Agent:
 
     def update_policy(self, trajectories, gamma):
         loss = torch.tensor([0]).float().cuda()
-        optim = torch.optim.Adam(self.policy_net.parameters())
+        optim = torch.optim.Adam(self.policy_net.parameters(), lr=0.1)
         for trajectory in trajectories:
-            log_probs = 0
-            r_t = 0
-            advantage = 0
-            for t in range(len(trajectory)):
-                log_probs += torch.log(self.policy_net(trajectory[t]['s'])[0][trajectory[t]['u']])
-                # r_t += gamma * trajectory[t]['r']
+            for t in range(len(trajectory['rewards'])):
                 r_t = 0
-                for t_d in range(t, len(trajectory)):
-                    r_t += gamma ** (t_d - t) * trajectory[t_d]['r']
+                log_prob = trajectory['log_probs'][t]
+                temp = trajectory['rewards'][t:]
+                for i, reward in enumerate(temp):
+                    r_t += gamma**i * reward
+                # for t_d in range(t, len(trajectory)):
+                #     r_t += gamma ** (t_d - t) * trajectory['rewards'][t_d]
                 # advantage += torch.FloatTensor([r_t]).cuda() - self.baseline(trajectory[t]['s'])[0]
-                advantage += torch.FloatTensor([r_t]).cuda()
-            loss += -log_probs * advantage
-        loss = loss.mean()
+                advantage = torch.FloatTensor([r_t]).cuda()
+                loss += -log_prob * advantage
+                # loss.backward()
+            # loss += -log_probs * advantage
+        loss = loss/len(trajectories)
         loss.backward()
+        # print("\nBefore zerograd\n")
+        # for name, param in self.policy_net.named_parameters():
+        #     print(name, param.grad.data.sum())
         optim.step()
         optim.zero_grad()
+        # print("\nAfter zerograd\n")
+        # for name, param in self.policy_net.named_parameters():
+        #     print(name, param.grad.data.sum())
 
 
 def main():
     env = make_env('PongNoFrameskip-v4')
     policy_net = PolicyNet(env.observation_space.shape, env.action_space.n).to(torch.device('cuda'))
     base_net = Baseline(env.observation_space.shape).to(torch.device('cuda'))
-    # policy_net.load_state_dict(torch.load('./policynet'))
-    # base_net.load_state_dict(torch.load('./basenet'))
+    policy_net.load_state_dict(torch.load('./policynet'))
+    base_net.load_state_dict(torch.load('./basenet'))
     agent = Agent(policy_net, base_net)
-    agent.train(env, 10, 20, 0.98, 5)
-    torch.save(policy_net.state_dict(), './policynet')
-    torch.save(base_net.state_dict(), './basenet')
+    agent.train(env, 16, 20000, 0.98, 5)
+
 
 main()
